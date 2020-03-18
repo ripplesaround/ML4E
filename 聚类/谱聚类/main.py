16609bef9a671de6feb6069eed2be091260de3e6
Ripples <37956697+ripplesaround@users.noreply.github.com>
2020-03-18 09:53:57 +0800

    学习总是痛苦的，但却又是快乐的

--- 聚类/谱聚类/main.py
+++ 聚类/谱聚类/main.py
@@ -14,7 +14,6 @@ sys.path.append("../")
 import data_process
 import numpy as np
 import matplotlib.pyplot as plt
-from k_means import kmeans
 from sklearn.cluster import SpectralClustering
 from Spectral_Clustering import *
 
@@ -26,7 +25,9 @@ data, true_labels= data_process.read_file_list(path)
 data = np.array(data)
 print('读入数据完毕')
 
-test = mysc(3,data)
+test = mysc(3,data,0.1)
 test.cluster_plot()
 
+# todo 探究gamma的值对结果的影响
+# todo 探究取前k大还是前k小
 
