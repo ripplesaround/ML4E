a6bcdfa64cf51713b187d61464e2385ef606d772
Ripples <37956697+ripplesaround@users.noreply.github.com>
2020-03-25 14:07:06 +0800

    学习总是痛苦的，又是快乐的

--- SVM/SVM.py
+++ SVM/SVM.py
@@ -7,3 +7,30 @@ Email: ripplesaround@sina.com
 date: 2020/3/25 13:38
 desc: 
 '''
+import sys
+import copy
+import data_process
+import numpy as np
+import matplotlib.pyplot as plt
+
+class svm:
+    def __init__(self,data,true_labels,K = 3):
+        self.data = data
+        self.true_labels = true_labels
+        self.K = K  # 共有几类
+        self.NUM = len(data)
+        self.train_data = []
+        self.test_data = []
+        for i in range(self.K):
+            temp = []
+            temp1 = []
+            temp_choice = np.random.choice(a=50,size = 40,replace = False)
+            temp_choice += i * (self.NUM//self.K)
+            for i in range(i*(self.NUM//self.K),(i+1)*self.NUM//self.K):
+                if i in temp_choice:
+                    temp.append(self.data[i])
+                else:
+                    temp1.append(self.data[i])
+            print(len(temp),len(temp1))
+
+
--- SVM/main.py
+++ SVM/main.py
@@ -13,11 +13,14 @@ sys.path.append("../")
 import data_process
 import numpy as np
 import matplotlib.pyplot as plt
+from SVM import *
 
 K = 3
 # 数据集规模
 N = 150
-path = '../iris/iris.data'
+path = 'iris/iris.data'
 data, true_labels= data_process.read_file_list(path)
 data = np.array(data)
 print('读入数据完毕')
+
+test = svm(data, true_labels)
