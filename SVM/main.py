7a115d32da0332f6ebee2ac3606367218a47b1de
Ripples <37956697+ripplesaround@users.noreply.github.com>
2020-03-25 13:40:20 +0800

    学习总是痛苦的，又是快乐的

--- SVM/main.py
+++ SVM/main.py
@@ -7,3 +7,17 @@ Email: ripplesaround@sina.com
 date: 2020/3/25 13:39
 desc: 
 '''
+import sys
+import copy
+sys.path.append("../")
+import data_process
+import numpy as np
+import matplotlib.pyplot as plt
+
+K = 3
+# 数据集规模
+N = 150
+path = '../iris/iris.data'
+data, true_labels= data_process.read_file_list(path)
+data = np.array(data)
+print('读入数据完毕')
